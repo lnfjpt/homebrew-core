e8b3b5cb018f9161a9aac1c94a94dd604719048a
nengli.ln <nengli.ln@alibaba-inc.com>
2020-09-07 10:54:55 +0800

    udpate boost

--- Formula/boost.rb
+++ Formula/boost.rb
@@ -1,95 +1,96 @@
-class Boost < Formula
+class BoostAT155 < Formula
   desc "Collection of portable C++ source libraries"
-  homepage "https://www.boost.org/"
-  url "https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2"
-  mirror "https://dl.bintray.com/homebrew/mirror/boost_1_73_0.tar.bz2"
-  sha256 "4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402"
+  homepage "https://www.boost.org"
+  url "https://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.bz2"
+  sha256 "fff00023dd79486d444c8e29922f4072e1d451fc5a4d2b6075852ead7f2b7b52"
   license "BSL-1.0"
-  head "https://github.com/boostorg/boost.git"
-
-  livecheck do
-    url "https://www.boost.org/feed/downloads.rss"
-    regex(/>Version v?(\d+(?:\.\d+)+)</i)
-  end
+  revision 1
 
   bottle do
     cellar :any
     rebuild 1
-    sha256 "21b7a8b746e98a39c299b5db595f97ff24cb498fee5c3c38be690bcc2510137b" => :catalina
-    sha256 "72159a6229ced562aa26100ce1c72f2ec3070cd6fe6e894e912abd9acc1b76bb" => :mojave
-    sha256 "f7e7f920d91f6b47a99f342a1414b992f012c49441e62b5587502d53cb31af7b" => :high_sierra
+    sha256 "2db1cc42c4810d7fdbe7dd7778fc51d37b829d8d39a8e5dc34a9867f7f619445" => :catalina
+    sha256 "655c9b514a797113af2e4199457ccb9dd8d8e0364f227390f0ca54b254439f2a" => :mojave
+    sha256 "15894f998719ef4130d2dea076accadb709a6d5d0809114452f5175585ccd454" => :high_sierra
+    sha256 "16a7e98e578adbf8c353bc868b9cd98e80b41928329743dba1b54ee53d76295c" => :sierra
   end
 
-  depends_on "icu4c"
+  keg_only :versioned_formula
 
-  uses_from_macos "bzip2"
-  uses_from_macos "zlib"
+  disable! because: :versioned_formula
+
+  # Patches boost::atomic for LLVM 3.4 as it is used on OS X 10.9 with Xcode 5.1
+  # https://github.com/Homebrew/homebrew/issues/27396
+  # https://github.com/Homebrew/homebrew/pull/27436
+  patch :p2 do
+    url "https://github.com/boostorg/atomic/commit/6bb71fdd.diff?full_index=1"
+    sha256 "1574ef5c1c3ec28cf3786e40e4a8608f2bbb1c426ef2f14a2515e7a1a9313fab"
+  end
+
+  patch :p2 do
+    url "https://github.com/boostorg/atomic/commit/e4bde20f.diff?full_index=1"
+    sha256 "fa6676d83993c59e3566fff105f7e99c193a54ef7dba5c3b327ebdb5b6dcba37"
+  end
+
+  # Patch fixes upstream issue reported here (https://svn.boost.org/trac/boost/ticket/9698).
+  # Will be fixed in Boost 1.56 and can be removed once that release is available.
+  # See this issue (https://github.com/Homebrew/homebrew/issues/30592) for more details.
+
+  patch :p2 do
+    url "https://github.com/boostorg/chrono/commit/143260d.diff?full_index=1"
+    sha256 "96ba2f3a028df323e9bdffb400cc7c30c0c67e3d681c8c5a867c40ae0549cb62"
+  end
 
   # Fix build on Xcode 11.4
-  patch do
+  patch :p2 do
     url "https://github.com/boostorg/build/commit/b3a59d265929a213f02a451bb63cea75d668a4d9.patch?full_index=1"
     sha256 "04a4df38ed9c5a4346fbb50ae4ccc948a1440328beac03cb3586c8e2e241be08"
-    directory "tools/build"
-  end
-
-  # Fix build on 64-bit arm
-  patch do
-    url "https://github.com/boostorg/build/commit/456be0b7ecca065fbccf380c2f51e0985e608ba0.patch?full_index=1"
-    sha256 "e7a78145452fc145ea5d6e5f61e72df7dcab3a6eebb2cade6b4cfae815687f3a"
-    directory "tools/build"
+    directory "tools/build/v2"
   end
 
   def install
-    # Force boost to compile with the desired compiler
+    # Patch boost::serialization for Clang
+    # https://svn.boost.org/trac/boost/raw-attachment/ticket/8757/0005-Boost.S11n-include-missing-algorithm.patch
+    inreplace "boost/archive/iterators/transform_width.hpp",
+      "#include <boost/iterator/iterator_traits.hpp>",
+      "#include <boost/iterator/iterator_traits.hpp>\n#include <algorithm>"
+
+    # Force boost to compile using the appropriate GCC version.
     open("user-config.jam", "a") do |file|
       file.write "using darwin : : #{ENV.cxx} ;\n"
     end
 
-    # libdir should be set by --prefix but isn't
-    icu4c_prefix = Formula["icu4c"].opt_prefix
-    bootstrap_args = %W[
-      --prefix=#{prefix}
-      --libdir=#{lib}
-      --with-icu=#{icu4c_prefix}
-    ]
+    # We specify libdir too because the script is apparently broken
+    bargs = %W[--prefix=#{prefix} --libdir=#{lib} --without-icu]
 
     # Handle libraries that will not be built.
-    without_libraries = ["python", "mpi"]
+    without_libraries = ["mpi", "python"]
 
     # Boost.Log cannot be built using Apple GCC at the moment. Disabled
     # on such systems.
     without_libraries << "log" if ENV.compiler == :gcc
 
-    bootstrap_args << "--without-libraries=#{without_libraries.join(",")}"
+    bargs << "--without-libraries=#{without_libraries.join(",")}"
 
-    # layout should be synchronized with boost-python and boost-mpi
     args = %W[
       --prefix=#{prefix}
       --libdir=#{lib}
       -d2
       -j#{ENV.make_jobs}
-      --layout=tagged-1.66
+      --layout=tagged
       --user-config=user-config.jam
-      -sNO_LZMA=1
-      -sNO_ZSTD=1
       install
       threading=multi,single
       link=shared,static
     ]
 
-    # Boost is using "clang++ -x c" to select C compiler which breaks C++14
-    # handling using ENV.cxx14. Using "cxxflags" and "linkflags" still works.
-    args << "cxxflags=-std=c++14"
-    args << "cxxflags=-stdlib=libc++" << "linkflags=-stdlib=libc++" if ENV.compiler == :clang
-
-    system "./bootstrap.sh", *bootstrap_args
-    system "./b2", "headers"
+    system "./bootstrap.sh", *bargs
     system "./b2", *args
   end
 
   def caveats
     s = ""
-    # ENV.compiler doesn't exist in caveats. Check library availability
+    # ENV.compiler doesn"t exist in caveats. Check library availability
     # instead.
     if Dir["#{lib}/libboost_log*"].empty?
       s += <<~EOS
@@ -103,12 +104,12 @@ class Boost < Formula
   test do
     (testpath/"test.cpp").write <<~EOS
       #include <boost/algorithm/string.hpp>
+      #include <boost/version.hpp>
       #include <string>
       #include <vector>
       #include <assert.h>
       using namespace boost::algorithm;
       using namespace std;
-
       int main()
       {
         string str("a,b");
@@ -117,10 +118,13 @@ class Boost < Formula
         assert(strVec.size()==2);
         assert(strVec[0]=="a");
         assert(strVec[1]=="b");
+
+        assert(strcmp(BOOST_LIB_VERSION, "1_55") == 0);
+
         return 0;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-std=c++14", "-stdlib=libc++", "-o", "test"
+    system ENV.cxx, "test.cpp", "-std=c++1y", "-I#{include}", "-L#{lib}", "-lboost_system", "-o", "test"
     system "./test"
   end
 end
